4332d5fb4861deaedbc62bf4e11a3793fea0cce3
Rodrigo Lizaran <rlizaran97@gmail.com>
2022-01-27 16:59:20 -1000

    Completed

---
+++ Level 1/sqlQueries.sql
@@ -0,0 +1,10 @@
+SELECT * FROM review_id_table;
+SELECT * FROM products;
+SELECT * FROM customers;
+SELECT * FROM vine_table;
+
+
+DROP TABLE review_id_table;
+DROP TABLE products;
+DROP TABLE customers;
+DROP TABLE vine_table;
